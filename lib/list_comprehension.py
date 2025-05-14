75ec337f450a74ab201cdfa3117d68eea4074689
robertkutwa <robertkutwa0@gmail.com>
2025-05-14 09:33:55 +0300

    comprehensions-and-generators

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+# lib/list_comprehension.py
 
-def make_exclamation(sentence_list):
-    pass
+def return_evens(nums):
+    return [num for num in nums if num % 2 == 0]
+
+def make_exclamation(sentences):
+    return [sentence + "!" for sentence in sentences]
